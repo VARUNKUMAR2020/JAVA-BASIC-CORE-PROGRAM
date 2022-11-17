6c4cb57e71366d6590b4b61d6fa1938f40c8d5bf
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-17 17:06:04 +0530

    Add: Coin Flip using Random

--- BasicJavaPrograms.java
+++ BasicJavaPrograms.java
@@ -1,5 +1,28 @@
 public class BasicJavaPrograms {
     public static void main(String[] args) throws Exception {
-        System.out.println("Java - Basic Programs: ");
+        System.out.println("Java - Basic Core Programs: ");
+
+        System.out.println("COIN FLIP");
+        double tails= 0;
+        double heads = 0;
+        for (int i = 1; i <= 10; i++) {
+            double flip = Math.random(); //Random-To get value 0.0 to 0.9
+            System.out.println("coin " + flip);
+
+            // condition 
+            if (flip < 0.5) {
+                tails++;
+            } else {
+                heads++;
+            }
+        }
+
+        // Calculating the percentage:
+        double percentageheads = (heads / 10) * 100;
+        double percentagetails = (tails / 10) * 100;
+
+        // Display of the percentage:
+        System.out.println("No.of.Heads "+heads+ " percentage is " + percentageheads);
+        System.out.println("No.of.Tails "+tails+" percentage is " + percentagetails);
     }
 }
