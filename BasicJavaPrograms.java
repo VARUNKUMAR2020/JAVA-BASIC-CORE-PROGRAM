660c998b8905110e6cb674625028d897cccf2f90
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-21 16:45:48 +0530

    Add: Swapping two number without temporary variable

--- BasicJavaPrograms.java
+++ BasicJavaPrograms.java
@@ -1,5 +1,13 @@
 public class BasicJavaPrograms {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Java - Basic Programs: ");
-    }
+   public static void main(String[] args) {
+      System.out.println("Java - Basic Programs: ");
+      int numberOne, numberTwo;
+      numberOne = 10;
+      numberTwo = 20;
+      System.out.println("Before Swapping\nx = " + numberOne + "\ny = " + numberTwo);
+      numberOne = numberOne + numberTwo; // sum of two values
+      numberTwo = numberOne - numberTwo; // subtract sum from second to get swap
+      numberOne = numberOne - numberTwo;
+      System.out.println("After Swapping without temp variable\nx = " + numberOne + "\ny = " + numberTwo);
+   }
 }
